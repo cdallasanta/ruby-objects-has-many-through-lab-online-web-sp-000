fe73b70ef77f1776a4749f18669d97232cc83702
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-14 20:55:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,4 +4,20 @@ class Patient
 
   def initialize(name)
     @name = name
-    
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.patient = self}
+  end
+
+  def doctors
+    doctors = []
+    appointments.each{|appt| doctors << appt.doctor}
+    doctors
+  end
+end
