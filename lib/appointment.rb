526fb90f583095d881a46e142dad843ceea502aa
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-14 20:52:44 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,12 @@
 class Appointment
+  @@all = []
+  attr_accessor :date, :doctor, :patient
+
+  def initialize(date, doctor, patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+end
